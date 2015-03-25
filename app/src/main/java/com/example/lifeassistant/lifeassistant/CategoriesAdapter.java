2741484daedb9c876300a7847691e3af638af35e
Gabrijela91 <gabrijelaa91@gmail.com,>
2015-03-25 23:03:34 +0100

    Kategorije
    Kategorije

---
+++ app/src/main/java/com/example/lifeassistant/lifeassistant/CategoriesAdapter.java
@@ -0,0 +1,51 @@
+package com.example.lifeassistant.lifeassistant;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Computer on 25.3.2015..
+ */
+public class CategoriesAdapter extends BaseAdapter {
+    LayoutInflater inflater;
+
+    Context ctxt;
+
+    CategoriesAdapter(Context ctxt, int resource, String[] Categories) {
+        super();
+        this.ctxt = ctxt;
+        this.inflater = LayoutInflater.from(ctxt);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = this.inflater.inflate(R.layout.list_item_categories, null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.textView);
+        textView.setText(Categories[position]);
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return Categories.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    static final String[] Categories = new String[]{"Shop list", "Homework", "Workout", "Other"};
+
+}
+
--- app/src/main/java/com/example/lifeassistant/lifeassistant/MainActivity.java
+++ app/src/main/java/com/example/lifeassistant/lifeassistant/MainActivity.java
@@ -1,39 +1,30 @@
 package com.example.lifeassistant.lifeassistant;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class MainActivity extends Activity
+        implements AdapterView.OnItemSelectedListener {
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        GridView g = (GridView) findViewById(R.id.gridView);
+        g.setAdapter(new CategoriesAdapter(this, android.R.layout.simple_list_item_1, CategoriesAdapter.Categories));
+        g.setOnItemSelectedListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
